0138d0e76c5b9133638383897723cb33eb614438
Haseeb Nawaz <educationalskills083@gmail.com>
2025-12-25 02:59:56 -0500

    Add mini Python project for practice

---
+++ mini project.py
@@ -0,0 +1,10 @@
+# Simple Student Score Evaluator
+
+score = int(input("Enter your score: "))
+
+if score >= 80:
+    print("Excellent")
+elif score >= 60:
+    print("Good")
+else:
+    print("Needs Improvement")
