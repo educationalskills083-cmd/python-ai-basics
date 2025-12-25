132265173123c17ca9d31d668bab679bb29cc445
Haseeb Nawaz <educationalskills083@gmail.com>
2025-12-25 01:57:55 -0500

    Add lists and tuples example

---
+++ basics/basics/liststuples.py
@@ -0,0 +1,16 @@
+# List example
+fruits = ["apple", "banana", "mango"]
+
+print("All fruits:", fruits)
+print("First fruit:", fruits[0])
+
+fruits.append("orange")
+print("After adding fruit:", fruits)
+
+# Tuple example
+colors = ("red", "green", "blue")
+print("Colors:", colors)
+
+# Loop with list
+for fruit in fruits:
+    print("Fruit:", fruit)
